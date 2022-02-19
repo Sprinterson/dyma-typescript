bfe939e9ed9e867a396119e42a823214cb319b3c
Charles Raviaud <charles.raviaud@protonmail.com>
2022-02-19 19:07:40 +0100

    feat: types introduction

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
-const test: string = '123';
+let prenom: string = "Jean";
+prenom = "Paul";
+prenom = "Patrick";
+console.log(prenom);
+// prenom = 42; => Type '42' is not assignable to type 'string'.ts
+
+let actif: boolean;
+actif = true;
+actif = false;
+console.log(actif);
+// actif = 42; => Type '42' is not assignable to type 'boolean'.ts
